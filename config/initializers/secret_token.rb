6b960e7f86626f35f9d17e15351c3ef64cf6bf80
Josh Steiner <josh@jsteiner.me>
2014-04-07 20:20:34 -0400

    rails new taskr -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taskr::Application.config.secret_key_base = 'c34aec183be0d3e22c2fc2e3474034d0d3abf713be8dbccc5a7aa5943de5fb3cb4babfe3d85093f3d3b6f59b05ae6fbbd9234da81b772b7b8774b625ee7af2a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Taskr::Application.config.session_store :cookie_store, key: '_taskr_session'
